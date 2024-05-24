c8c56ef654f870cc342e28bacbddb516d10cf0eb
German <german2009pro@mail.ru>
2024-05-24 18:14:21 +0300

    переписан BASE_URL доработка

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://back-pindie-back.nomoredomainswork.ru/api'
+export const BASE_URL = ["https://back-pindie-back.nomoredomainswork.ru/api"]
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
